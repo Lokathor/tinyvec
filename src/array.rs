dea4143fb605132c1ea92a222c8b0f29fe6e7e91
Lokathor <zefria@gmail.com>
2020-01-24 00:38:45 -0700

    must_use on Array methods

--- src/array.rs
+++ src/array.rs
@@ -39,10 +39,12 @@ impl<T: Default, const N: usize> Array for [T; N] {
   type Item = T;
   const CAPACITY: usize = N;
   #[inline(always)]
+  #[must_use]
   fn as_slice(&self) -> &[T] {
     &*self
   }
   #[inline(always)]
+  #[must_use]
   fn as_slice_mut(&mut self) -> &mut [T] {
     &mut *self
   }
@@ -55,10 +57,12 @@ macro_rules! impl_array_for_len {
       type Item = T;
       const CAPACITY: usize = $len;
       #[inline(always)]
+      #[must_use]
       fn as_slice(&self) -> &[T] {
         &*self
       }
       #[inline(always)]
+      #[must_use]
       fn as_slice_mut(&mut self) -> &mut [T] {
         &mut *self
       }
