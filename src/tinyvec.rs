62ea3a60cf725264ee38dac8efa1365ac88cd915
LoRd_MuldeR <mulder@posteo.de>
2026-08-20 21:55:21 +0200

    Added TinyVec::with_initial_len() function and associated tests. (#224)

--- src/tinyvec.rs
+++ src/tinyvec.rs
@@ -684,6 +684,33 @@ impl<A: Array> TinyVec<A> {
     }
   }
 
+  /// Makes a default-initialized TinyVec with the given initial length.
+  ///
+  /// If the requested length is less than or equal to the array capacity you
+  /// get an inline vec. If it's greater than you get a heap vec.
+  /// ```
+  /// # use tinyvec::*;
+  /// let t = TinyVec::<[u8; 10]>::with_initial_len(5);
+  /// assert!(t.is_inline());
+  /// assert_eq!(t.len(), 5);
+  ///
+  /// let t = TinyVec::<[u8; 10]>::with_initial_len(20);
+  /// assert!(t.is_heap());
+  /// assert_eq!(t.len(), 20);
+  /// ```
+  #[inline]
+  #[must_use]
+  pub fn with_initial_len(len: usize) -> Self
+  where
+    A::Item: Clone,
+  {
+    if len <= A::CAPACITY {
+      TinyVec::Inline(ArrayVec::from_array_len(A::default(), len))
+    } else {
+      TinyVec::Heap(vec![A::Item::default(); len])
+    }
+  }
+
   /// Converts a `TinyVec<[T; N]>` into a `Box<[T]>`.
   ///
   /// - For `TinyVec::Heap(Vec<T>)`, it takes the `Vec<T>` and converts it into
